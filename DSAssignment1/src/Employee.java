ad7c6ff941adb67976e9def8639b9c507a9a890c
Manzi <manzeyjoseph@gmail.com>
2020-10-18 13:52:43 +0100

    updated the employee class with a taxNumber Field

--- DSAssignment1/src/Employee.java
+++ DSAssignment1/src/Employee.java
@@ -1,6 +1,7 @@
 
 public class Employee {
 	
+	private int taxNumber;
 	private String firstName;
 	private String lastName;
 	private String jobTitle;
@@ -8,7 +9,8 @@ public class Employee {
 	private String address;
 	private String email;
 	
-	public Employee(String firstName,String lastName, String jobTitle, String phoneNumber,String address, String email) {
+	public Employee(int taxNumber, String firstName,String lastName, String jobTitle, String phoneNumber,String address, String email) {
+		this.taxNumber = taxNumber;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.jobTitle = jobTitle;
@@ -17,55 +19,5 @@ public class Employee {
 		this.email = email;
 		
 	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getJobTitle() {
-		return jobTitle;
-	}
-
-	public void setJobTitle(String jobTitle) {
-		this.jobTitle = jobTitle;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
 
 }
