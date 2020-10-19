685f3878a3fd2c3d80fdb1e4570ec3ff6ed241db
Manzi <manzeyjoseph@gmail.com>
2020-10-19 06:29:05 +0100

    updated the Employee class

--- DSAssignment1/src/Employee.java
+++ DSAssignment1/src/Employee.java
@@ -5,19 +5,112 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private String jobTitle;
-	private String phoneNumber;
-	private String address;
+	private int salary;
 	private String email;
 	
-	public Employee(int taxNumber, String firstName,String lastName, String jobTitle, String phoneNumber,String address, String email) {
-		this.taxNumber = taxNumber;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.jobTitle = jobTitle;
-		this.phoneNumber = phoneNumber;
-		this.address = address;
-		this.email = email;
+	public Employee(int taxNumber, String firstName,String lastName, String jobTitle, int salary, String email) {
 		
+		// *********VALIDATION OF DATA**************//
+		
+		if((taxNumber>=100) && (taxNumber<=200)) {
+			this.taxNumber = taxNumber;
+		}else {
+			this.taxNumber = 100;
+		}
+		
+		if(firstName.length()<=15) {
+			this.firstName = firstName;
+		}else {
+			this.firstName = firstName.substring(0,15);
+		}
+	
+		if(lastName.length()<=20) {
+			this.lastName = lastName;
+		}else {
+			this.lastName = lastName.substring(0,20);
+		}
+		
+		if(jobTitle.length()<=50) {
+			this.jobTitle = jobTitle;
+		}else {
+			this.jobTitle = jobTitle.substring(0,50);
+		}
+		
+		if((salary>=20000) && (salary<=150000)) {
+			this.salary = salary;
+		}else {
+			this.salary = 20000;
+		}
+		
+		if(email.length()<=50) {
+			this.email = email;
+		}else {
+			this.email = email.substring(0,50);
+		}
+		
+	}
+	
+	
+	//*******GETTERS FUCNTIONS********//
+	
+	public int getTaxNumber() {
+		return taxNumber;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	
+	//*******SETTERS FUCNTIONS***********//
+	
+	public void setTaxNumber(int taxNumber) {
+		this.taxNumber = taxNumber;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String toString() {
+		return "Tax Number: "  +  taxNumber + ", " +
+				"First Name : "  +  firstName + ", " +
+				"Last Name : "  + lastName + ", " +
+				"Job Tittle : "  +  jobTitle + " " +
+				"Salary : "  +  salary + ", " +
+				"Email :"  +  email;
 	}
 
 }
